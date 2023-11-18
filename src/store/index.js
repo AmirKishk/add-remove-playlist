fc49c567a90826a81ec6b4b267caec73c4d8714e
Amir Kishk <kishk.amir@gmail.com>
2023-11-18 19:36:47 +0100

    commit finished pashed 1 - playlists working

---
+++ src/store/index.js
@@ -0,0 +1,43 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const moviesSlice = createSlice({
+  name: "movie",
+  initialState: [],
+  reducers: {
+    addMovie(state, action) {
+      state.push(action.payload);
+    },
+    removeMovie(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
+const songsSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    //TYPE: name of the slice: 'song' + '/' + name of the function: 'addSong' --> song/addSong
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    //TYPE: name of the slice: 'song' + '/' + name of the function: 'removeSong' --> song/removeSong
+    removeSong(state, action) {
+      // action. payload === string, the song we remove
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    songs: songsSlice.reducer,
+    movies: moviesSlice.reducer,
+  },
+});
+
+export { store };
+export const { addSong, removeSong } = songsSlice.actions;
+export const { addMovie, removeMovie } = moviesSlice.actions;
